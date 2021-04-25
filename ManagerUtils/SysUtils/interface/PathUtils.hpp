622da443879e6dd5201b2f50c9754e82706f8e70
sun51027 <arashikattun48@gmail.com>
2021-04-25 20:25:35 +0800

    Draw Tool

---
+++ ManagerUtils/SysUtils/interface/PathUtils.hpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+*
+*  Filename    : PathUtils.hpp
+*  Description : C++ Wrapper functions for path interaction
+*  Author      : Yi-Mu "Enoch" Chen [ ensc@hep1.phys.ntu.edu.tw ]
+*
+*  Powered Mainly powered by boost/filesystem and POSIX
+*
+*******************************************************************************/
+#ifndef MANAGERUTILS_SYSUTILS_PATHUTILS
+#define MANAGERUTILS_SYSUTILS_PATHUTILS
+
+/*******************************************************************************
+*   Automatically including string enhancing libraries
+*******************************************************************************/
+#include "/home/zero/work/ManagerUtils/Common/interface/STLUtils/StringUtils.hpp"
+
+/*******************************************************************************
+*   Commonly used Paths
+*******************************************************************************/
+#include "/home/zero/work/ManagerUtils/SysUtils/interface/PathUtils/CommonPath.hpp"
+
+/*******************************************************************************
+*   Globbing related functions
+*******************************************************************************/
+#include "/home/zero/work/ManagerUtils/SysUtils/interface/PathUtils/Glob.hpp"
+
+/*******************************************************************************
+*   String generation functions
+*******************************************************************************/
+#include "/home/zero/work/ManagerUtils/SysUtils/interface/PathUtils/Generate.hpp"
+
+
+#endif/* end of include guard: MANAGERUTILS_SYSUTILS_PATHUTILS */
